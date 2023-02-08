0f5e4052ff8e103932bbb7043ed035967229fe30
eyyüp erdoğan <eyyuperdogan@eyyup-MacBook-Air.local>
2023-02-08 18:54:49 +0300

    kotlincoroutineretrofit

---
+++ app/src/main/java/com/atilsamancioglu/kotlincoroutineretrofit/RecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.atilsamancioglu.kotlincoroutineretrofit
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.atilsamancioglu.kotlincoroutineretrofit.databinding.RowLayoutBinding
+
+class RecyclerViewAdapter(private val cryptoList : ArrayList<CryptoModel>, private val listener : Listener) : RecyclerView.Adapter<RecyclerViewAdapter.RowHolder>() {
+
+    interface Listener {
+        fun onItemClick(cryptoModel: CryptoModel)
+    }
+    private val colors: Array<String> = arrayOf("#13bd27","#29c1e1","#b129e1","#d3df13","#f6bd0c","#a1fb93","#0d9de3","#ffe48f")
+
+    class RowHolder(val binding : RowLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RowHolder {
+        val itemBinding = RowLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RowHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return cryptoList.count()
+    }
+
+    override fun onBindViewHolder(holder: RowHolder, position: Int) {
+
+        holder.itemView.setOnClickListener {
+            listener.onItemClick(cryptoList.get(position))
+        }
+        holder.itemView.setBackgroundColor(Color.parseColor(colors[position % 8]))
+        holder.binding.textName.text = cryptoList.get(position).currency
+        holder.binding.textPrice.text = cryptoList.get(position).price
+    }
+
+
+}
+
